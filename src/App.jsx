d604e081f82a3bad1b681adf0b2ff713f0d5364b
pravin-git <pravinpawar@gmail.com>
2026-09-25 20:01:30 +0530

    atm strike

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,14 @@ import OptionChainMatrix from './OptionChainMatrix'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   const handleOptionAction = (payload) => {
     console.log("Action received:", payload);
   };
 
   return (
     <>
-      <section id="center">
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count} Pravin
-        </button>
-      </section>
-
+      
       <div className="ticks"></div>
       <OptionChainMatrix
         atmStrike={18000}
@@ -29,9 +19,6 @@ function App() {
         onAction={handleOptionAction}
       />
 
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
